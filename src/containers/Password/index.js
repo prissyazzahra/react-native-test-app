fc8167a4b6941094c61e2b508f65aef6a4cd63d8
Prissy Azzahra <prissy.azzahra@gmail.com>
2020-05-27 16:44:08 +0700

    [feature] add password error handling

--- src/containers/Password/index.js
+++ src/containers/Password/index.js
@@ -1,26 +1,51 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 
-import { Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Text, View, TouchableOpacity, TextInput, ToastAndroid } from 'react-native';
 
 import styles from './styles';
 
-export default function Password({ navigation, route }) {
-  const [password, setPassword] = React.useState('');
-  const { username } = route.params;
+class Password extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      password: '',
+    }
+  }
 
-  return (
-      <View style={styles.container}>
-        <Text style={styles.instructions}>Please enter your GitHub password.</Text>
-        <View>
-          <TextInput placeholder="Password" textContentType="password" secureTextEntry={true} style={styles.text} />
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate(('Welcome'), { username })}
-          >
-            <Text style={styles.buttonText}>Submit</Text>
-          </TouchableOpacity>
+  handleSubmit = () => {
+    const { username } = this.props.route.params;
+
+    if (!this.state.password) {
+      return ToastAndroid.show("Please enter your password.", ToastAndroid.SHORT);
+    } else if (this.state.password.length < 8) {
+      return ToastAndroid.show("Invalid password.", ToastAndroid.SHORT);
+    }
+    return this.props.navigation.navigate(('Welcome'), { username })
+  }
+  render() {
+  
+    return (
+        <View style={styles.container}>
+          <Text style={styles.instructions}>Please enter your GitHub password.</Text>
+          <View>
+            <TextInput 
+              onChangeText={e => this.setState({ password: e })}
+              placeholder="Password"
+              textContentType="password"
+              secureTextEntry={true}
+              style={styles.text}  
+            />
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.handleSubmit()}
+            >
+              <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-  );
+    );
+  }
 }
+
+export default Password;
